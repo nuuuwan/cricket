a68bb0ac115edb878e83a07fb7ead7d74770ee6c
nuuuwan <nuuuwan@gmail.com>
2023-07-07 10:22:05 +0530

    Moved to teams_last_games

---
+++ src/cricket/reports/teams_last_games.py
@@ -0,0 +1,27 @@
+from cricket.core.ODI import ODI
+
+
+def teams_last_games(team_name):
+    from cricket.core.Team import TEAM_IDX, Team
+
+    odis = ODI.load_list_for_team(team_name)
+    prev_year = None
+    for odi in odis:
+        other_team = odi.team1 if odi.team2 == team_name else odi.team2
+        if other_team not in TEAM_IDX:
+            continue
+
+        year = odi.year
+        if prev_year != year:
+            print('')
+        prev_year = year
+
+        icon = '✅' if odi.winner == team_name else '❌'
+        print(
+            f"{odi.date} ({odi.time_weight:.2f}) {icon} {Team.load(other_team)}"
+        )
+
+
+if __name__ == "__main__":
+    TEAM = 'Sri Lanka'
+    teams_last_games(TEAM)
