91bb7c6abf1b5cc27699fdeedc9462d6d8439275
nuuuwan <nuuuwan@gmail.com>
2023-07-07 10:21:56 +0530

    Use ODI.load_idx2_by_teams(gender)

--- src/cricket/analysis/head_to_head.py
+++ src/cricket/analysis/head_to_head.py
@@ -4,29 +4,8 @@ from cricket.core.ODI import ODI
 
 
 @cache
-def head_to_head_odis():
-    odis = ODI.loadAll()
-    idx = {}
-    for odi in odis:
-        team1 = odi.team1
-        team2 = odi.team2
-        if team1 not in idx:
-            idx[team1] = {}
-        if team2 not in idx[team1]:
-            idx[team1][team2] = []
-        if team2 not in idx:
-            idx[team2] = {}
-        if team1 not in idx[team2]:
-            idx[team2][team1] = []
-
-        idx[team1][team2].append(odi)
-        idx[team2][team1].append(odi)
-    return idx
-
-
-@cache
-def head_to_head_odds():
-    idx = head_to_head_odis()
+def head_to_head_odds(gender):
+    idx = ODI.load_idx2_by_teams(gender)
     idx2 = {}
     for team1 in idx:
         idx2[team1] = {}
