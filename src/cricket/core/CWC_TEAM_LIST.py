bb321cf9ab6846c69cfbc1b2b31ea6f489b02aa9
nuuuwan <nuuuwan@gmail.com>
2023-07-07 13:44:10 +0530

    CWC_TEAM_LIST

---
+++ src/cricket/core/CWC_TEAM_LIST.py
@@ -0,0 +1,12 @@
+CWC_TEAM_LIST = [
+    'Afghanistan',
+    'Australia',
+    'Bangladesh',
+    'England',
+    'India',
+    'Netherlands',
+    'New Zealand',
+    'Pakistan',
+    'South Africa',
+    'Sri Lanka',
+]
