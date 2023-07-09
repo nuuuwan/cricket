f77e1d0482c0d323c70428ff72e6296b83ddc3f9
nuuuwan <nuuuwan@gmail.com>
2023-07-09 08:30:18 +0530

    various

--- src/cricket/reports/india_advantage.py
+++ src/cricket/reports/india_advantage.py
@@ -8,14 +8,14 @@ def main():
         for odi in odis
         if odi.country == 'India'
         and odi.loser is not None
-        and not odi.did_team_play('India')
+        and odi.did_team_play('India')
     ]
     n_india = {}
     n_win_india = {}
     for odi in filtered_odis:
         winner = odi.winner
         loser = odi.loser
-        w = odi.time_weight
+        w = 1  # odi.time_weight
 
         for team_name in [winner, loser]:
             n_india[team_name] = n_india.get(team_name, 0) + w
@@ -24,7 +24,7 @@ def main():
     for team_name, n_i_india in sorted(n_india.items(), key=lambda x: x[1]):
         n_i_win_india = n_win_india.get(team_name, 0)
         p_i_win_india = n_i_win_india / n_i_india
-        print(f"{p_i_win_india:.1%} {Team.load(team_name)} ({n_i_india})")
+        print(f"{p_i_win_india:.0%} {Team.load(team_name)}")
 
 
 if __name__ == '__main__':
